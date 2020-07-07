4a02ae7f2c74e367874962bacd97f5241d38adb3
Augusto Lessa <augustolessa_c@hotmail.com>
2020-07-07 19:51:42 -0300

    Adds the basic code for posts and modal components

--- app/components/posts.js
+++ app/components/posts.js
@@ -1,4 +1,13 @@
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import { A } from "@ember/array";
 
 export default class PostsComponent extends Component {
+  array = A([1,2,3,4,5]);
+  @tracked isModalVisible = false;
+
+  @action toggleModal() {
+    this.isModalVisible = !this.isModalVisible;
+  }
 }
