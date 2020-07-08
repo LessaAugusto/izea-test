43eee27b8c35c09d4191f4c0b76f871fc2f20dc4
Augusto Lessa <augustolessa_c@hotmail.com>
2020-07-08 11:39:00 -0300

    Updates the posts component to show the data that's passed in

--- app/components/posts.js
+++ app/components/posts.js
@@ -1,11 +1,22 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
-import { A } from "@ember/array";
 
 export default class PostsComponent extends Component {
-  array = A([1,2,3,4,5]);
+  /**
+   * This component is going to control the modal state (the user is going to open the modal
+   * clicking on a post).
+   */
   @tracked isModalVisible = false;
+
+  /**
+   * This variable basically creates an array with the size equals to the total number of pages we should
+   * show. It populates this array with values from 1 to the total number of pages, like so:
+   * [1,2,3,4,..., {{totalNumberOfPages}} ]
+   */
+   pages = Array.from(
+    Array(this.args.totalNumberOfPages), (_, i) => i + 1
+  );
 
   @action toggleModal() {
     this.isModalVisible = !this.isModalVisible;
