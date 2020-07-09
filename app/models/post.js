4368ce94ac28841342b8791ae46395b66fd6d145
Augusto Lessa <augustolessa_c@hotmail.com>
2020-07-09 19:28:42 -0300

    Adds an alias to the userId property of the user model

--- app/models/post.js
+++ app/models/post.js
@@ -1,7 +1,9 @@
 import Model, { attr, belongsTo } from "@ember-data/model";
+import { alias } from "@ember/object/computed";
 
 export default class PostModel extends Model {
   @belongsTo("user") userId;
+  @alias("userId") user;
   @attr("string") title;
   @attr("string") body;
 }
