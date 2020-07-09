e10fbdedacb980303a9ec5e8c08e8f8c943db49d
Augusto Lessa <augustolessa_c@hotmail.com>
2020-07-09 19:23:22 -0300

    Adds user model to the project

---
+++ app/models/user.js
@@ -0,0 +1,11 @@
+import Model, { attr } from "@ember-data/model";
+
+export default class UserModel extends Model {
+  @attr("string") name;
+  @attr("string") username;
+  @attr("string") email;
+  @attr address;
+  @attr("string") phone;
+  @attr("string") website;
+  @attr company;
+}
